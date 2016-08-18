e9b57bc526e7a2336fd36c748223b2c3c073a728
Suhael Akhtar <suhael.akhtar@maersk.com>
2016-08-18 15:39:53 +0100

    multiproject build

---
+++ gradleService/src/main/java/com/suhael/gradle/service/Person.java
@@ -0,0 +1,14 @@
+package com.suhael.gradle.service;
+
+public class Person {
+
+    private String name = "suhael";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- gradleWebApp/src/main/java/com/suhael/gradle/HelloWorldServlet.java
+++ gradleWebApp/src/main/java/com/suhael/gradle/HelloWorldServlet.java
@@ -1,11 +1,12 @@
 package com.suhael.gradle;
 
-import java.io.IOException;
+import com.suhael.gradle.service.Person;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class HelloWorldServlet extends HttpServlet {
 
@@ -15,5 +16,6 @@ public class HelloWorldServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         resp.getOutputStream().write("Hello World.".getBytes());
+        resp.getOutputStream().write(new Person().getName().getBytes());
     }
 }
